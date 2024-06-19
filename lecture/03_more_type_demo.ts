65a7b71b40eac8776cbb5cd5ab67b14e7867f5cf
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-06-20 06:08:50 +0700

    Update

---
+++ lecture/03_more_type_demo.ts
@@ -0,0 +1,39 @@
+interface Address {
+  city: string;
+}
+
+interface Post {
+  title: string;
+  text?: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  isActive: boolean;
+  phone: string | number | null | undefined;
+  website?: string;
+  role: "ADMIN" | "USER";
+  address: Address;
+  foods: string[];
+  posts: Post[];
+  greet: () => void;
+}
+
+const user: User = {
+  id: 0,
+  name: "Haynes",
+  isActive: true,
+  phone: 34234334,
+  role: "ADMIN",
+  address: {
+    city: "Chiang Mai",
+  },
+  foods: ["rice", "pizza"],
+  posts: [],
+  greet: function () {
+    console.log(`Hello, my name is ${(this as User).name}`);
+  },
+};
+
+user.greet();
